263d24dd4179a33deecdbd512bb3dfe540c83ee0
serendipity019 <navagio09@gmail.com>
2025-01-19 13:58:24 +0200

    Examples from lesson 2

---
+++ lesson2/eventsInput.js
@@ -0,0 +1,8 @@
+function echo() {
+    const inputTxt = document.getElementById("inputTxt")
+    const val = inputTxt.value
+
+    const helloText = document.getElementById("helloText")
+    helloText.innerHTML = val
+
+}
