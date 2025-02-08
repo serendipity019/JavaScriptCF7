f786caa58887cfc206fe10ae212aca8bd71698ec
serendipity019 <navagio09@gmail.com>
2025-02-08 18:27:24 +0200

    feat: fresh copies of array index(add, delete, update)

---
+++ arrays/18-immutable-arrays.js
@@ -0,0 +1,30 @@
+const numbers = [1, 2, 3, 4];
+
+//Modifies the initial array 
+numbers.push(5);
+numbers.splice(4, 0, 5);
+
+//These below create new arrays
+function addToArrayEnd(arr, num) {
+    return [...arr, num];
+}
+
+function addToArrayStart(arr, num) {
+    return [num, ...arr];
+}
+
+function addToArrayAtIndex(arr, num, index) {
+    return [...arr.slice(0, index), num, ...arr.slice(index)]; 
+}
+
+// Updates
+numbers.splice(3, 1, 8);
+
+const updateArray = (arr, newValue) => arr.map(item => newValue); // if we want all the items become update
+
+//if we want to make update in specific position with index. Frexh copy
+const updateOneItem = (arr, index, newValue) => arr.map((item, i) => (i === index) ? newValue : item);
+
+const updatedNumbers = updateOneItem(numbers, 2, 17);
+console.log(updatedNumbers);
+
